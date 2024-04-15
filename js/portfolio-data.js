c74979b2d89b4c0b581603d62f8308d41053c0a7
Alejandro <alejhandro.de@gmail.com>
2024-04-14 21:52:33 -0600

    adding portafolios page

---
+++ js/portfolio-data.js
@@ -0,0 +1,37 @@
+var portfolio_data = {
+    nails: [
+        {
+            name: 'uñas_1',
+            url: 'images/portfolio/unas_2.jpeg',
+            show: true
+        },
+        {
+            name: 'uñas_1',
+            url: 'images/portfolio/unas_1.jpeg',
+            show: true
+        },
+        
+        {
+            name: 'uñas_1',
+            url: 'images/portfolio/unas_3.jpeg',
+            show: true
+        },
+        {
+            name: 'uñas_1',
+            url: 'images/portfolio/unas_4.jpeg',
+            show: true
+        },
+    ],
+    lashes: [
+        {
+            name: 'pestañas_1',
+            url: 'images/portfolio/pestanas_1.jpeg',
+            show: true
+        },
+        {
+            name: 'pestañas_2',
+            url: 'images/portfolio/pestanas_2.jpeg',
+            show: true
+        },
+    ]
+}
